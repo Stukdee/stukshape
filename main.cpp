fdf185b8379e9558b4addf5a38418f76af8724bd
Stukdee <354536332@qq.com>
2026-08-05 22:03:41 +0800

    再次修改main.cpp，github怎么没有同步呢？？？

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include "stukshape.h"
+import stukshape;
+
 int main(void){
-	stukshape::show_window();
+	stukshape::Stukshape theMain;
+	theMain.init();
+	theMain.showAllInformation();
+	stukshape::Window theWindow(400,400,"hello");
+	theWindow.show();
+	theWindow.showDisplayModeInformation();
+	while (!theWindow.shouldBeClose()){}
 	return 0;
 }
